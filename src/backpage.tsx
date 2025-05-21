03c1d8304fad227f8789d4ddd05613ebae74b06c
rio <g2554303@cc.kyoto-su.ac.jp>
2025-05-21 17:38:53 +0900

    木の表示やページ移動のボタン表示を作った

---
+++ src/backpage.tsx
@@ -0,0 +1,11 @@
+//import { useNavigate } from "react-router";
+//
+//export function Backpage() {
+//  const navigate = useNavigate();
+//
+//  return (
+//    <div>
+//    <button onClick={() => navigate("App/")}>ホームに戻る</button>
+//    </div>
+//  );
+//}
--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,34 +1,39 @@
 import React from 'react'
-//import readline from 'readline';
-
-
-let points: number = 300;
-//const rl = readline.createInterface({
-//    input: process.stdin,
-//    output: process.stdout
-//  });
-//  
-//  rl.question('ポイント数は？', (points) => {
-//    rl.close();
-//  });
+import { useTaskContext } from '../contexts/TaskContext';
+import { useNavigate } from "react-router";
 
 const Tree = () => { 
     let treeImage = '';
+    const {point,tasks} = useTaskContext();
+    const navigate = useNavigate();
 
-    if ((points) >= 30) {
-      treeImage = '/tree3.png';
-    } else if ((points) >= 20) {
-      treeImage = '/tree2.png';
-    } else if ((points) >= 10) {
-      treeImage = '/tree1.png';
+    console.log('point:', point); // デバッグ用
+    console.log('tasks:', tasks); // デバッグ用
+    if ((point) >= 30) {
+      treeImage = '/image3.png';
+    } else if ((point) >= 20) {
+      treeImage = '/image2.png';
+    } else if ((point) >= 10) {
+      treeImage = '/image1.png';
     }
   return (
     <div>  
         {treeImage && (
-        <img src={treeImage} alt="木" style={{ height: '200px' }} />
+        <img src={treeImage} alt="木" style={{ height: '850px' }} />
       )}
+      <button className='backbutton' onClick={() => navigate("/")}>ホームに戻る</button>
     </div>
   )
 }
 
 export default Tree
+//
+//export function Backpage() {
+//  const navigate = useNavigate();
+//
+//  return (
+//    <div>
+//    <button onClick={() => navigate("App/")}>ホームに戻る</button>
+//    </div>
+//  );
+//}
